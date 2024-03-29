87dde3a07f88db1aa234e61a86ad4d2da0aab8a9
Sarah Lamond <101301017+sarahlamond@users.noreply.github.com>
2024-03-29 19:47:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeToThumbnail() {
+    var image = document.getElementById("rosalia-image");
+    image.src = "rosalia singing.png";
+    image.style.width = "850px";
+    image.style.height = "auto"; 
+}
+
+function changeToImage() {
+    var image = document.getElementById("rosalia-image");
+    image.src = "rosalia flower.png";
+}
+
